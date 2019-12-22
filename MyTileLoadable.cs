6ceed61c6f813c4f12cba95dc00e176c5b30c104
hamstar0 <webutek@gmail.com>
2019-12-22 01:14:25 -0800

    v1.0.0 - Added ExtendedTileHooks reporting of "non-gameplay" tile kills during house create

---
+++ MyTileLoadable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using HamstarHelpers.Classes.Loadable;
+using HamstarHelpers.Services.Hooks.ExtendedHooks;
+
+
+namespace HouseKits {
+	class HouseKitsTileLoadable : ILoadable {
+		public bool IsCreatingHouse { get; private set; }
+
+
+
+		////////////////
+
+		public void OnModsLoad() {
+			HouseKitsAPI.OnPreHouseCreate( ( tileX, tileY, item ) => {
+				this.IsCreatingHouse = true;
+				return true;
+			} );
+
+			HouseKitsAPI.OnPostHouseCreate( ( tileX, tileY, item ) => {
+				this.IsCreatingHouse = false;
+			} );
+
+			if( ExtendedTileHooks.NonGameplayKillTileCondition == null ) {
+				ExtendedTileHooks.NonGameplayKillTileCondition = () => this.IsCreatingHouse;
+			} else {
+				Func<bool> oldHook = ExtendedTileHooks.NonGameplayKillTileCondition;
+				ExtendedTileHooks.NonGameplayKillTileCondition = () => {
+					return this.IsCreatingHouse || oldHook();
+				};
+			}
+		}
+
+
+		public void OnModsUnload() { }
+
+		public void OnPostModsLoad() { }
+	}
+}
