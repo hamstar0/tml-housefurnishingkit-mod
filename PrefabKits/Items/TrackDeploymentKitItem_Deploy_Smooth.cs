660a227a68dbeca906c5992e82946d51da6460d2
hamstar0 <webutek@gmail.com>
2020-11-05 18:19:42 -0800

    v1.2.0 - Fixed and finished track kit smoothing

--- PrefabKits/Items/TrackDeploymentKitItem_Deploy_Smooth.cs
+++ PrefabKits/Items/TrackDeploymentKitItem_Deploy_Smooth.cs
@@ -8,31 +8,19 @@ using HamstarHelpers.Helpers.Debug;
 namespace PrefabKits.Items {
 	public partial class TrackDeploymentKitItem : ModItem {
 		private static void SmoothPath( IList<(int tileX, int tileY)> path ) {
-			int prevHighNodeIdx = -1;
+			int prevHighNodeIdx = 0;
 
-			for( int i=0; i<path.Count; i++ ) {
-				if( prevHighNodeIdx == -1 ) {
-					prevHighNodeIdx = i;
-					continue;
+			for( int i=1; i<path.Count; i++ ) {
+				if( path[prevHighNodeIdx].tileY < path[i].tileY ) {
+					int? nextHighIdx = TrackDeploymentKitItem.SmoothPathTestAhead( path, prevHighNodeIdx );
+
+					if( nextHighIdx.HasValue ) {
+						TrackDeploymentKitItem.SmoothPathBetween( path, prevHighNodeIdx, nextHighIdx.Value );
+						i = nextHighIdx.Value;
+					}
 				}
-
-				(int tileX, int tileY) prevNode = path[prevHighNodeIdx];
-				(int tileX, int tileY) currNode = path[i];
-				if( prevNode.tileY >= currNode.tileY ) {
-					continue;
-				}
-
-				int? nextHighIdx = TrackDeploymentKitItem.SmoothPathTestAhead( path, prevHighNodeIdx );
 
 				prevHighNodeIdx = i;
-
-				if( !nextHighIdx.HasValue ) {
-					continue;
-				}
-
-				TrackDeploymentKitItem.SmoothPathBetween( path, prevHighNodeIdx, nextHighIdx.Value );
-
-				i = nextHighIdx.Value + 1;
 			}
 		}
 
@@ -44,12 +32,24 @@ namespace PrefabKits.Items {
 				(int tileX, int tileY) currNode = path[i];
 				(int tileX, int tileY) testNode = ( currNode.tileX, prevNode.tileY );
 
-				if( !TrackDeploymentKitItem.TrackValid.Check(testNode.tileX, testNode.tileY) ) {
-					return null;
+				// Found a matching height node
+				if( currNode.tileY == prevNode.tileY ) {
+//int t = 60*10;
+//Timers.RunUntil( () => {
+//	Dust.QuickDust(new Point(testNode.tileX, testNode.tileY), Color.Lime);
+//	return t-- > 0;
+//}, true );
+					return i;
 				}
 
-				if( currNode.tileY == prevNode.tileY ) {
-					return i;
+				// Not a clear path
+				if( !TrackDeploymentKitItem.TrackValid.Check(testNode.tileX, testNode.tileY) ) {
+//int t = 60 * 10;
+//Timers.RunUntil( () => {
+//	Dust.QuickDust(new Point(testNode.tileX, testNode.tileY), Color.Red);
+//	return t-- > 0;
+//}, true );
+					return null;
 				}
 			}
 
@@ -60,10 +60,11 @@ namespace PrefabKits.Items {
 		private static void SmoothPathBetween( IList<(int tileX, int tileY)> path, int fromNodeIdx, int toNodeIdx ) {
 			(int tileX, int tileY) fromNode = path[ fromNodeIdx ];
 
-			for( int i=fromNodeIdx+1; i<path.Count; i++ ) {
+			for( int i=fromNodeIdx+1; i<toNodeIdx; i++ ) {
 				(int tileX, int tileY) currNode = path[i];
 
 				currNode.tileY = fromNode.tileY;
+
 				path[i] = currNode;
 			}
 		}
